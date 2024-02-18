0a89bdca6b31297a71633daf0f82991f891263ed
Febin Rukfan <32138498+FebinRukfan@users.noreply.github.com>
2024-02-18 14:23:51 -0500

    ADDED Data Preference and Compose Navigation

--- app/src/main/java/com/febinrukfan/news365/presentation/mainactivity/MainActivity.kt
+++ app/src/main/java/com/febinrukfan/news365/presentation/mainactivity/MainActivity.kt
@@ -1,41 +1,34 @@
-package com.febinrukfan.news365
+package com.febinrukfan.news365.presentation.mainactivity
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.WindowCompat
-import com.febinrukfan.news365.domain.usecases.AppEntryUseCases
-import com.febinrukfan.news365.presentation.onboarding.OnBoardingScreen
+import com.febinrukfan.news365.presentation.navgraph.NavGraph
 import com.febinrukfan.news365.ui.theme.News365Theme
 import dagger.hilt.android.AndroidEntryPoint
-import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
-    @Inject
-    lateinit var appEntryUseCases: AppEntryUseCases
+    private val viewModel by viewModels<MainActivityViewModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         WindowCompat.setDecorFitsSystemWindows(window, false)
-
+        installSplashScreen().apply {
+            setKeepOnScreenCondition(condition = { viewModel.splashCondition.value })
+        }
         setContent {
-            News365Theme(
-                dynamicColor = false
-            ) {
+            News365Theme(dynamicColor = false) {
                 Box(modifier = Modifier.background(MaterialTheme.colorScheme.background)) {
-                    OnBoardingScreen()
+                 NavGraph(startDestination = viewModel.startDestination.value)
                 }
             }
         }
--- app/src/main/java/com/febinrukfan/news365/presentation/onboarding/OnBoardingViewModel.kt
+++ app/src/main/java/com/febinrukfan/news365/presentation/onboarding/OnBoardingViewModel.kt
@@ -3,9 +3,11 @@ package com.febinrukfan.news365.presentation.onboarding
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.febinrukfan.news365.domain.usecases.AppEntryUseCases
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
+@HiltViewModel
 class OnBoardingViewModel @Inject constructor(
     private val appEntryUseCases: AppEntryUseCases
 ) : ViewModel() {
